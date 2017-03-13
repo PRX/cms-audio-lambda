a82070a2c6618ba4e81c176cf02f82edad7c5c41
cavis <ryancavis@gmail.com>
2017-03-13 17:08:50 -0600

    Remove request; fail downloads where content-length doesn't match

--- test/processor/download-test.js
+++ test/processor/download-test.js
@@ -27,12 +27,23 @@ describe('processor-download', () => {
     });
   });
 
+  it('throws missing download errors', () => {
+    return processor.download(null).then(
+      (data) => { throw 'should have gotten an error'; },
+      (err) => {
+        expect(err.message).to.match(/no url set/i);
+        expect(err.fromDownload).to.be.falsey;
+      }
+    );
+  });
+
   it('handles s3 errors', () => {
     let url = 's3://foo/bar.jpg';
     return processor.download(url).then(
       (data) => { throw 'should have gotten an error'; },
       (err) => {
         expect(err.message).to.match(/access denied/i);
+        expect(err.fromDownload).to.be.true;
       }
     );
   });
@@ -43,6 +54,7 @@ describe('processor-download', () => {
       (data) => { throw 'should have gotten an error'; },
       (err) => {
         expect(err.message).to.match(/got 404 for/i);
+        expect(err.fromDownload).to.be.true;
       }
     );
   });
@@ -53,6 +65,7 @@ describe('processor-download', () => {
       (data) => { throw 'should have gotten an error'; },
       (err) => {
         expect(err.message).to.match(/ENOTFOUND/i);
+        expect(err.fromDownload).to.be.true;
       }
     );
   });
@@ -63,6 +76,7 @@ describe('processor-download', () => {
       (data) => { throw 'should have gotten an error'; },
       (err) => {
         expect(err.message).to.match(/key does not exist/i);
+        expect(err.fromDownload).to.be.true;
       }
     );
   });
@@ -73,8 +87,31 @@ describe('processor-download', () => {
       (data) => { throw 'should have gotten an error'; },
       (err) => {
         expect(err.message).to.match(/unrecognized url format/i);
+        expect(err.fromDownload).to.be.true;
       }
     );
+  });
+
+  it('throws content-length mismatch errors', () => {
+    nock('http://foo.bar').get('/mismatch.mp3').reply(200, '--mp3--', {'Content-Length': 11});
+    return processor.download('http://foo.bar/mismatch.mp3').then(
+      (data) => { throw 'should have gotten an error'; },
+      (err) => {
+        expect(err.message).to.match(/expected 11 bytes/i);
+        expect(err.message).to.match(/got 7/i);
+        expect(err.fromDownload).to.be.falsey;
+      }
+    );
+  });
+
+  it('is okay with matching content-length', () => {
+    nock('http://foo.bar').get('/okay.mp3').reply(200, '--mp3--', {'Content-Length': 7});
+    return processor.download('http://foo.bar/okay.mp3').then(data => {
+      expect(data.name).to.equal('okay.mp3');
+      expect(data.localPath).to.equal('/tmp/okay.mp3');
+      expect(data.contentType).to.be.undefined;
+      expect(helper.readSize(data.localPath)).to.equal(7);
+    });
   });
 
 });
